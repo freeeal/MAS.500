bdb30b7673a1c776e6dbb062f9a0ae85b3f4fb37
Alan Yan <acyanlight@gmail.com>
2017-11-17 23:56:33 -0500

    add unit test

--- hw2/main.py
+++ hw2/main.py
@@ -9,51 +9,54 @@ import logging
 logging.basicConfig(filename="logs.txt", level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-def get_api_key():
-    logger.info('Getting API key from config file...')
-    config = configparser.ConfigParser()
-    try:
-        config.read('../config.ini')
-        return config['MEDIACLOUD']['api_key']
-    except (SystemExit, KeyboardInterrupt):
-        raise
-    except Exception:
-        logger.error('Failed to read file', exc_info=True)
+## source for "test-driven" style: https://github.com/rahulbot/Programming-Style-Examples/blob/master/test-driven.py)
+class MediaCloud:
 
-def make_mc_connection(api_key):
-    mc_connection = mediacloud.api.MediaCloud(mc_api_key)
-    if mc_connection is None:
-        logger.error('Cannot make connection.')
-    return mc_connection
+    def get_api_key(self):
+        logger.info('Getting API key from config file...')
+        config = configparser.ConfigParser()
+        try:
+            config.read('../config.ini')
+            self.api_key = config['MEDIACLOUD']['api_key']
+        except (SystemExit, KeyboardInterrupt):
+            raise
+        except Exception:
+            logger.error('Failed to read file', exc_info=True)
 
-mc_api_key = get_api_key()
-mc = make_mc_connection(mc_api_key)
+    def make_mc_connection(self):
+        self.mc_connection = mediacloud.api.MediaCloud(self.api_key)
+        if self.mc_connection is None:
+            logger.error('Cannot make connection.')
+        # return self.mc_connection
 
-# request is call to Media Cloud API
-# 1st arg is tuple of search term strings
-# 2nd argument is date tuple (Y, M, D)
-# 3rd argument is date tuple (Y, M, D)
-def handle_request(search_tuple, start_date, end_date):
-    logger.info('Handling request...')
-    logger.info('Search term(s): %s', search_tuple)
-    logger.info('Start date: %s', start_date)
-    logger.info('End date: %s', end_date)
-    result = mc.sentenceCount(str(search_tuple), solr_filter=[mc.publish_date_query(datetime.date(start_date[0], start_date[1], start_date[2]), datetime.date(end_date[0], end_date[1], end_date[2])) ])
-    logger.info('Return result: %s', result)
-    return result
+    # request is call to Media Cloud API
+    # 1st arg is tuple of search term strings
+    # 2nd argument is date tuple (Y, M, D)
+    # 3rd argument is date tuple (Y, M, D)
+    def handle_request(self, search_tuple, start_date, end_date):
+        self.get_api_key()
+        self.make_mc_connection()
+        logger.info('Handling request...')
+        logger.info('Search term(s): %s', search_tuple)
+        logger.info('Start date: %s', start_date)
+        logger.info('End date: %s', end_date)
+        result = self.mc_connection.sentenceCount(str(search_tuple), solr_filter=[self.mc_connection.publish_date_query(datetime.date(start_date[0], start_date[1], start_date[2]), datetime.date(end_date[0], end_date[1], end_date[2])) ])
+        logger.info('Return result: %s', result)
+        return result
+
+## Make MediaCloud object
+media_cloud = MediaCloud()
 
 ## Beginner HW: 2016 election research question
-# trump_res = mc.sentenceCount('(Trump)', solr_filter=[mc.publish_date_query(datetime.date(2016, 9, 1), datetime.date(2016, 9, 30)) ])
-# clinton_res = mc.sentenceCount('(Clinton)', solr_filter=[mc.publish_date_query(datetime.date(2016, 9, 1), datetime.date(2016, 9, 30)) ])
-trump_res = handle_request(('Trump'), (2016, 9, 1), (2016, 9, 30))
-clinton_res = handle_request(('Clinton'), (2016, 9, 1), (2016, 9, 30))
+trump_res = media_cloud.handle_request(('Trump'), (2016, 9, 1), (2016, 9, 30))
+clinton_res = media_cloud.handle_request(('Clinton'), (2016, 9, 1), (2016, 9, 30))
 print(trump_res['count']) #1917105
 print(clinton_res['count']) #1267285
 # "Did US Mainstream Media sources talk about Trump or Clinton more in September 2016?" --> Trump
 
 ## Intermediate HW: abortion research question
-res_2015 = mc.sentenceCount('(abortion)', solr_filter=[mc.publish_date_query(datetime.date(2015, 1, 1), datetime.date(2015, 12, 31)) ])
-res_2016 = mc.sentenceCount('(abortion)', solr_filter=[mc.publish_date_query(datetime.date(2016, 1, 1), datetime.date(2016, 12, 31)) ])
+res_2015 = media_cloud.handle_request(('abortion'), (2015, 1, 1), (2015, 12, 31))
+res_2016 = media_cloud.handle_request(('abortion'), (2016, 1, 1), (2016, 12, 31))
 print(res_2015['count']) #607293
 print(res_2016['count']) #833234
 # "Was abortion talked about more in 2015 or 2016?" --> 2016
--- hw2/test.py
+++ hw2/test.py
@@ -1,27 +1,13 @@
+# Verify that we can make a call to the MediaCloud API successfully
 import unittest
+from main import MediaCloud
 
+# source: https://github.com/mitmedialab/MediaCloud-API-Client/blob/master/mediacloud/test/apitest.py
 class MediaCloudTest(unittest.TestCase):
 
-    def setUp(self):
-        self.response = ElectionResults('election_results_test_file.csv')
-
-    def testLoad(self):
-        self.results.load()
-        assert self.results is not None
-        assert self.results.file is not None
-        assert len(self.results.all_lines) > 0
-
-    def testStateCount(self):
-        self.results.load()
-        state_count = self.results.state_count()
-        assert state_count==2
-
-    def testStates(self):
-        self.results.load()
-        names = self.results.states()
-        assert len(names)==2
-        assert names[0]=='Alaska'
-        assert names[1]=='Alabama'
+    def test_handle_request(self):
+        self.response = MediaCloud().handle_request(('Trump'), (2016, 9, 1), (2016, 9, 30))
+        assert self.response['count'] > 0
 
 # if this file is run directly, run the tests
 if __name__ == "__main__":
